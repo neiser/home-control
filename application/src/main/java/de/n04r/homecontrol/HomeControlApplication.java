bfc665d004866c58d7058cd9d51b0bdde57d176f
Andreas Grub <andreas.grub@qaware.de>
2021-03-03 15:31:23 +0100

    Add skeleton HomeControl Spring Boot Application

---
+++ application/src/main/java/de/n04r/homecontrol/HomeControlApplication.java
@@ -0,0 +1,13 @@
+package de.n04r.homecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeControlApplication.class, args);
+	}
+
+}
---
+++ application/src/test/java/de/n04r/homecontrol/HomeControlApplicationTests.java
@@ -0,0 +1,13 @@
+package de.n04r.homecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
